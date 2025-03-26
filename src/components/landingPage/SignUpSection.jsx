f0b50c69c14b29d4bfebbd4caf09d34518e03de6
Diego <diegoooliveira237@gmail.com>
2025-03-26 12:43:05 -0300

    ✨ feat(register): preencher email do formulário com valor da landing page

--- src/components/landingPage/SignUpSection.jsx
+++ src/components/landingPage/SignUpSection.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SignUpSection() {
+  const [email, setEmail] = useState("");
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
+
   return (
-    <div className="h-12 p-1 bg-white rounded-sm flex">
+    <form className="h-12 p-1 bg-white rounded-sm flex">
       <input
+        required
         className="h-full p-2 text-black rounded-sm mr-2"
         type="email"
         placeholder="Enter your email"
+        value={email}
+        onChange={handleChange}
+        aria-label="Email address"
       />
-      <Link className="h-full p-2 rounded-sm bg-violet-6" to="/register">
+      <Link
+        className="h-full p-2 bg-violet-6"
+        to={email && email.includes("@") ? "/register" : "#"}
+        state={{ email }}
+      >
         Sign up for MyFitApp
       </Link>
-    </div>
+    </form>
   );
 }
 
